44c5d8388b45e0db9e7180527169b64a8b491ec5
liangyun <895423140@qq.com>
2017-10-11 17:58:29 +0800

    添加node版本源码 Signed-off-by: liangyun <895423140@qq.com>

--- README.md
+++ README.md
@@ -1,21 +1,39 @@
-微信小程序转阿里小程序
-环境配置：
+# 微信小程序转阿里小程序 #
+## java版本 ##
+### 环境配置： ###
 	jre 1.7-64
-文件:
+### 使用方法： ###
+	安装指定jre 1.7-64环境；
+	下载runfile文件夹；
+	复制微信小程序的源码一份；
+	在使用前可以根据自己需要修改wx2ali.txt配置，
+	来进行修改工作目录；
+	然后 双击translation.bat；
+	文件就会修改完成；
+## node版本 ##
+### 环境配置： ###
+	node.js
+### 安装 ###
+	npm i wx2ali -g
+### 使用 ###
+	在使用前可以根据自己需要修改module下的wx2ali.txt配置，
+	来进行修改工作目录
+	之后，命令行直接输入:
+	wx2ali
+	等待处理完成。
+	
+## 文件: ##
 	runfile
-		wx2ali.jar 程序包
-		wx2ali.txt 配置  程序包和配置在同一目录
-		translation.bat 启动文件
-	src	源码	
-	lib commons-io-1.3.2.jar
+	  --wx2ali.jar //程序包
+	  --wx2ali.txt //配置
+	  --translation.bat //启动文件
+	src	//源码
+	node
+	  --wx2ali.txt //配置
+ 	  --package.json
+	  --index.js //源码
+	lib 
+	  --commons-io-1.3.2.jar
 	
-使用方法：
-	安装指定jre 1.7-64环境
-	下载runfile文件夹
-	复制微信小程序的源码一份
-	填写配置文件
-	然后 双击translation.bat
-	文件就会修改完成
-	
-有问题可以联系895423140@qq.com
+有问题可以联系895423140@qq.com 或者提交issue。
 	
---
+++ node/index.js
@@ -0,0 +1,303 @@
+#!/usr/bin/env node
+var fs = require("fs");
+var path = require("path");
+var readline = require('linebyline');
+var arg = process.argv;
+var dir = path.dirname(arg[1]);
+var configpath = path.resolve(dir, "wx2ali.txt");
+var rl = readline(configpath);
+let Order = 0;
+/**
+* 将符合后缀的文件copy和修改后缀名为指定的后缀。e.g.
+* addUpdateSuffix(".wxml",".axml");将会copy指定目录下所有的.wxml后缀的文件为.axml后缀的文件
+*/
+const UPDATAANDCOPY = 1;
+/**
+* 删除指定后缀的文件 e.g. addUpdateSuffix("abc.xml")将会删除指定目录下所有的abc.xml
+*/
+const DELETEFILE = 2;
+/**
+* 将符合后缀的文件替换为指定的后缀 e.g.
+* addUpdateSuffix(".wxml",".axml");将会修改指定目录下所有的.wxml为.axml
+*/
+const UPDATESUFFIX = 3;
+/**
+* 微信小程序转阿里小程序 js文件主要是进行库名的修改，即'wx.'-->'my.'
+* 方法名替换使用addUpdateMethods()添加更换的方法 e.g.
+* addUpdateMethods("request","httpRequest") 将request转换成httpRequest
+* axml文件主要进行 属性名称的修改，即'wx:'-->'a:'
+*/
+const WX2ANT = 4;
+const config = {
+    dir: "",
+    suffix: [],
+    toSuffix: [],
+    methods: [],
+    toMethods: [],
+    JSRegexp: [],
+    JSToRegexp: [],
+    AXMLRegexp: [],
+    AXMLToRegexp: [],
+};
+var state = "";
+rl
+    .on('line', function (line, lineCount, byteCount) {
+        console.log(lineCount + ":" + line);
+        line = line.trim();
+        if (state === "") {
+            if ("JSmethod" === line) {
+                state = line
+            } else if ("JS" === line) {
+                state = line
+            } else if ("AXML" === line) {
+                state = line
+            } else if ("DIR" === line) {
+                state = line
+            } else if("OVER"===line){
+                main();
+            }
+        } else if ("end" === line) {
+            state = "";
+        } else if ("" === line) {
+        } else if ("JSmethod" === state) {
+            var aTob = line.split("--->");
+            addUpdateMethods(aTob[0], aTob[1]);
+            // if (config.methods.push(aTob[0]) != config.toMethods.push(aTob[1])) {
+            //     throw new Error("配置中的参数填写错误，请检查");
+            // }
+        } else if ("JS" === state) {
+            var aTob = line.split("--->");
+            addJSRegexp(aTob[0], aTob[1]);
+            // if (config.JSRegexp.push(aTob[0]) != config.JSToRegexp.push(aTob[1])) {
+            //     throw new Error("配置中的参数填写错误，请检查");
+            // }
+        } else if ("AXML" === state) {
+            var aTob = line.split("--->");
+            addAXMLRegexp(aTob[0], aTob[1]);
+            // if (config.AXMLRegexp.push(aTob[0]) != config.AXMLToRegexp.push(aTob[1])) {
+            //     throw new Error("配置中的参数填写错误，请检查");
+            // }
+        } else if ("DIR" === state) {
+            config.dir = line;
+            console.log("修改文件的路径切换到：" + line);
+        } else if("OVER"===line){
+            main();
+        }
+    })
+    .on('error', function (e) {
+        console.log(e);
+    });
+function main(){
+    addUpdateSuffix("wxml", "axml");//这边的order是UPDATESUFFIX 所以是修改后缀名 wxml->axml
+    addUpdateSuffix("wxss", "acss");//wxss->acss
+    setOrder(UPDATESUFFIX);
+    HandleFile(config.dir);
+    clearSuffix();
+    //	修改的文件后缀名
+    addUpdateSuffix("axml");//这边的order是WX2ANT 所以是修改该后缀名的文件
+    addUpdateSuffix("js");
+    setOrder(WX2ANT);
+    HandleFile(config.dir);
+}
+function setOrder(order) {
+    Order = order;
+}
+function addUpdateMethods(method, toMethod) {
+    config.methods.push(method);
+    config.toMethods.push(toMethod || "");
+}
+
+function clearMethod() {
+    config.methods = [];
+    config.toMethods = [];
+}
+
+// ----------------------------------- 要更新的后缀
+
+function addUpdateSuffix(suffix, toSuffix) {
+    config.suffix.push(suffix);
+    config.toSuffix.push(toSuffix || "");
+}
+
+function clearSuffix() {
+    config.suffix = [];
+    config.toSuffix = [];
+}
+
+// ----------------------------------- js要更新的正则表达式
+
+function addJSRegexp(suffix, toSuffix) {
+    config.JSRegexp.push(suffix);
+    config.JSToRegexp.push(toSuffix || "");
+}
+
+function clearJSRegexp() {
+    config.JSRegexp = [];
+    config.JSToRegexp = [];
+}
+
+// ----------------------------------- amxl要更新的正则表达式
+function addAXMLRegexp(suffix, toSuffix) {
+    config.AXMLRegexp.push(suffix);
+    config.AXMLToRegexp.push(toSuffix);
+}
+
+function clearAXMLRegexp() {
+    config.AXMLRegexp = [];
+    config.AXMLToRegexp = [];
+}
+// -----------------------------------
+function HandleFile(file) {
+  
+    try {
+        fs.accessSync(file)
+        let stat = fs.statSync(file);
+        if (stat.isFile()) {
+            switch (Order) {
+                case UPDATAANDCOPY:
+                    updataAndCopy(file);
+                    break;
+                case DELETEFILE:
+                    deleteFile(file);
+                    break;
+                case UPDATESUFFIX:
+                    updataSuffix(file);
+                    break;
+                case WX2ANT:
+                    wx2ant(file);
+                    break;
+                default:
+                    console.log("没有该种处理文件的方式");
+                    break;
+            }
+        } else if (stat.isDirectory()) {
+            let dirs = fs.readdirSync(file);
+            for (let i in dirs) {
+                HandleFile(path.resolve(file, dirs[i]));
+            }
+        }
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+
+/**
+* 将符合后缀的文件copy和修改后缀名为指定的后缀。e.g.
+* addUpdateSuffix(".wxml",".axml");将会copy指定目录下所有的.wxml后缀的文件为.axml后缀的文件
+* 
+* @param f
+*/
+function updataAndCopy(file) {
+    let index = isValid(file);
+    if (index == -1) {
+        return;
+    }
+    let suffix = config.suffix;
+    let toSuffix = config.toSuffix;
+    let newfile = file.replace(new RegExp(suffix[index] + "$"), toSuffix[index]);
+    try {
+        fs.copyFileSync(file, newfile);
+        console.log("文件复制：" + newfile);
+    } catch (e) {
+        console.log(e)
+        console.log("文件复制出错：" + e.getMessage());
+    }
+}
+
+/**
+ * 将符合后缀的文件替换为指定的后缀 e.g.
+ * addUpdateSuffix(".wxml",".axml");将会修改指定目录下所有的.wxml为.axml
+ * 
+ * @param f
+ */
+function updataSuffix(file) {
+    let index = isValid(file);
+    if (index == -1) {
+        return;
+    }
+    let suffix = config.suffix;
+    let toSuffix = config.toSuffix;
+    let newfile = file.replace(new RegExp(suffix[index] + "$"), toSuffix[index]);
+    try {
+        fs.renameSync(file, newfile);
+        console.log("文件修改后缀名：" + newfile);
+    } catch (e) {
+        console.log(e)
+        console.log("文件修改后缀名出错：" + newfile + "--" + e.getMessage());
+    }
+}
+
+/**
+ * 删除指定后缀的文件 e.g. addUpdateSuffix("abc.xml")将会删除指定目录下所有的abc.xml
+ * 
+ * @param f
+ */
+function deleteFile(file) {
+    try {
+        fs.accessSync(file);
+        fs.unlinkSync(file);
+        console.log("文件删除成功:" + path);
+    } catch (e) {
+        console.log(e);
+        console.log(file + "文件有误");
+    }
+}
+
+/**
+ * 微信小程序转阿里小程序 js文件主要是进行库名的修改，即'wx.'-->'my.'
+ * 方法名替换使用addUpdateMethods()添加更换的方法 e.g.
+ * addUpdateMethods("request","httpRequest") 将request转换成httpRequest
+ * axml文件主要进行 属性名称的修改，即'wx:'-->'a:'
+ * 
+ * @param f
+ */
+function wx2ant(file) {
+    if (path.extname(file) === ".js") {
+        let preffix = "(^|\\W+)wx\\.";
+        let toPreffix = "$1my.";
+        let methods = config.methods;
+        let toMethods = config.toMethods;
+        let JSRegexp = config.JSRegexp;
+        let JSToRegexp = config.JSToRegexp;
+        try {
+            let content = fs.readFileSync(file, "utf8");
+            for (let i in methods) {// 修改不一样的方法
+                content = content.replace(new RegExp(preffix + methods[i], "g"), toPreffix + toMethods[i]);
+            }
+            for (var i in JSRegexp) {// 修改不一样的方法
+                content = content.replace(new RegExp(JSRegexp[i], "g"), JSToRegexp[i]);
+            }
+            content = content.replace(new RegExp(preffix, "g"), toPreffix);// 统一修改未进行方法替换的前缀
+            fs.writeFileSync(file, content);
+            console.log("转换js文件：" + file);
+        } catch (e) {
+            console.log(e)
+            console.log("转换js文件出错：" + file);
+        }
+    } else if (path.extname(file) === ".axml") {
+        let AXMLRegexp = config.AXMLRegexp;
+        let AXMLToRegexp = config.AXMLToRegexp;
+        try {
+            let content = fs.readFileSync(file, "utf8");
+            for (let i in AXMLRegexp) {// 修改不一样的方法
+                content = content.replace(new RegExp(AXMLRegexp[i], "g"), AXMLToRegexp[i]);
+            }
+            fs.writeFileSync(file, content);
+            console.log("转换axml文件：" + file);
+        } catch (e) {
+            console.log(e)
+            console.log("转换axml文件出错：" + file);
+        }
+    }
+}
+
+function isValid(file) {
+    let suffix = config.suffix
+    for (var i in suffix) {
+        if (file.endsWith(suffix[i])) {
+            return i;
+        }
+    }
+    return -1;
+}
