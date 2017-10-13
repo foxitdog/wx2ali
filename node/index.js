9aaa90ba57a631b3258e9d70d2477b9d63023520
liangyun <895423140@qq.com>
2017-10-13 19:32:07 +0800

    v1.0.2 添加了json文件的适配 Signed-off-by: liangyun <895423140@qq.com>

--- node/index.js
+++ node/index.js
@@ -8,25 +8,24 @@ var configpath = path.resolve(dir, "wx2ali.txt");
 var rl = readline(configpath);
 let Order = 0;
 /**
-* 将符合后缀的文件copy和修改后缀名为指定的后缀。e.g.
-* addUpdateSuffix(".wxml",".axml");将会copy指定目录下所有的.wxml后缀的文件为.axml后缀的文件
-*/
+ * 将符合后缀的文件copy和修改后缀名为指定的后缀。e.g.
+ * addUpdateSuffix(".wxml",".axml");将会copy指定目录下所有的.wxml后缀的文件为.axml后缀的文件
+ */
 const UPDATAANDCOPY = 1;
 /**
-* 删除指定后缀的文件 e.g. addUpdateSuffix("abc.xml")将会删除指定目录下所有的abc.xml
-*/
+ * 删除指定后缀的文件 e.g. addUpdateSuffix("abc.xml")将会删除指定目录下所有的abc.xml
+ */
 const DELETEFILE = 2;
 /**
-* 将符合后缀的文件替换为指定的后缀 e.g.
-* addUpdateSuffix(".wxml",".axml");将会修改指定目录下所有的.wxml为.axml
-*/
+ * 将符合后缀的文件替换为指定的后缀 e.g.
+ * addUpdateSuffix(".wxml",".axml");将会修改指定目录下所有的.wxml为.axml
+ */
 const UPDATESUFFIX = 3;
 /**
-* 微信小程序转阿里小程序 js文件主要是进行库名的修改，即'wx.'-->'my.'
-* 方法名替换使用addUpdateMethods()添加更换的方法 e.g.
-* addUpdateMethods("request","httpRequest") 将request转换成httpRequest
-* axml文件主要进行 属性名称的修改，即'wx:'-->'a:'
-*/
+ * 微信小程序转阿里小程序 js文件主要是进行库名的修改，即'wx.'-->'my.' 方法名替换使用addUpdateMethods()添加更换的方法
+ * e.g. addUpdateMethods("request","httpRequest") 将request转换成httpRequest
+ * axml文件主要进行 属性名称的修改，即'wx:'-->'a:'
+ */
 const WX2ANT = 4;
 const config = {
     dir: "",
@@ -38,12 +37,13 @@ const config = {
     JSToRegexp: [],
     AXMLRegexp: [],
     AXMLToRegexp: [],
+	JSONRegexp:[],
+	JSONToRegexp:[]
 };
 var state = "";
 rl
     .on('line', function (line, lineCount, byteCount) {
-        console.log(lineCount + ":" + line);
-        line = line.trim();
+        line=line.replace(/#.*$/g, "").trim();
         if (state === "") {
             if ("JSmethod" === line) {
                 state = line
@@ -53,6 +53,8 @@ rl
                 state = line
             } else if ("DIR" === line) {
                 state = line
+            } else if ("JSON" === line) {
+            	state = line
             } else if("OVER"===line){
                 main();
             }
@@ -62,24 +64,18 @@ rl
         } else if ("JSmethod" === state) {
             var aTob = line.split("--->");
             addUpdateMethods(aTob[0], aTob[1]);
-            // if (config.methods.push(aTob[0]) != config.toMethods.push(aTob[1])) {
-            //     throw new Error("配置中的参数填写错误，请检查");
-            // }
         } else if ("JS" === state) {
             var aTob = line.split("--->");
             addJSRegexp(aTob[0], aTob[1]);
-            // if (config.JSRegexp.push(aTob[0]) != config.JSToRegexp.push(aTob[1])) {
-            //     throw new Error("配置中的参数填写错误，请检查");
-            // }
         } else if ("AXML" === state) {
             var aTob = line.split("--->");
             addAXMLRegexp(aTob[0], aTob[1]);
-            // if (config.AXMLRegexp.push(aTob[0]) != config.AXMLToRegexp.push(aTob[1])) {
-            //     throw new Error("配置中的参数填写错误，请检查");
-            // }
+        } else if ("JSON" === state) {
+            var aTob = line.split("--->");
+            addJSONRegexp(aTob[0], aTob[1]);
         } else if ("DIR" === state) {
             config.dir = line;
-            console.log("修改文件的路径切换到：" + line);
+            console.log("切换到修改文件路径：" + line);
         } else if("OVER"===line){
             main();
         }
@@ -88,14 +84,11 @@ rl
         console.log(e);
     });
 function main(){
-    addUpdateSuffix("wxml", "axml");//这边的order是UPDATESUFFIX 所以是修改后缀名 wxml->axml
-    addUpdateSuffix("wxss", "acss");//wxss->acss
+    addUpdateSuffix("wxml", "axml");// 这边的order是UPDATESUFFIX 所以是修改后缀名 wxml->axml
+    addUpdateSuffix("wxss", "acss");// wxss->acss
     setOrder(UPDATESUFFIX);
     HandleFile(config.dir);
     clearSuffix();
-    //	修改的文件后缀名
-    addUpdateSuffix("axml");//这边的order是WX2ANT 所以是修改该后缀名的文件
-    addUpdateSuffix("js");
     setOrder(WX2ANT);
     HandleFile(config.dir);
 }
@@ -146,6 +139,16 @@ function clearAXMLRegexp() {
     config.AXMLRegexp = [];
     config.AXMLToRegexp = [];
 }
+//----------------------------------- json要更新的正则表达式
+function addJSONRegexp(suffix, toSuffix) {
+    config.JSONRegexp.push(suffix);
+    config.JSONToRegexp.push(toSuffix);
+}
+
+function clearJSONRegexp() {
+    config.JSONRegexp = [];
+    config.JSONToRegexp = [];
+}
 // -----------------------------------
 function HandleFile(file) {
   
@@ -183,11 +186,11 @@ function HandleFile(file) {
 
 
 /**
-* 将符合后缀的文件copy和修改后缀名为指定的后缀。e.g.
-* addUpdateSuffix(".wxml",".axml");将会copy指定目录下所有的.wxml后缀的文件为.axml后缀的文件
-* 
-* @param f
-*/
+ * 将符合后缀的文件copy和修改后缀名为指定的后缀。e.g.
+ * addUpdateSuffix(".wxml",".axml");将会copy指定目录下所有的.wxml后缀的文件为.axml后缀的文件
+ * 
+ * @param f
+ */
 function updataAndCopy(file) {
     let index = isValid(file);
     if (index == -1) {
@@ -245,9 +248,8 @@ function deleteFile(file) {
 }
 
 /**
- * 微信小程序转阿里小程序 js文件主要是进行库名的修改，即'wx.'-->'my.'
- * 方法名替换使用addUpdateMethods()添加更换的方法 e.g.
- * addUpdateMethods("request","httpRequest") 将request转换成httpRequest
+ * 微信小程序转阿里小程序 js文件主要是进行库名的修改，即'wx.'-->'my.' 方法名替换使用addUpdateMethods()添加更换的方法
+ * e.g. addUpdateMethods("request","httpRequest") 将request转换成httpRequest
  * axml文件主要进行 属性名称的修改，即'wx:'-->'a:'
  * 
  * @param f
@@ -288,6 +290,20 @@ function wx2ant(file) {
         } catch (e) {
             console.log(e)
             console.log("转换axml文件出错：" + file);
+        }
+    } else if (path.extname(file) === ".json") {
+        let JSONRegexp = config.JSONRegexp;
+        let JSONToRegexp = config.JSONToRegexp;
+        try {
+            let content = fs.readFileSync(file, "utf8");
+            for (let i in JSONRegexp) {// 修改不一样的方法
+                content = content.replace(new RegExp(JSONRegexp[i], "g"), JSONToRegexp[i]);
+            }
+            fs.writeFileSync(file, content);
+            console.log("转换json文件：" + file);
+        } catch (e) {
+            console.log(e)
+            console.log("转换json文件出错：" + file);
         }
     }
 }
--- src/com/text/parser/Wx2Ant.java
+++ src/com/text/parser/Wx2Ant.java
@@ -3,15 +3,15 @@ package com.text.parser;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.nio.file.Path;
 import java.util.ArrayList;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.LineIterator;
 
 /**
- * 梁云-895423140@qq.com
- * 文件操作都是以File类来进行操作， 所谓的后缀也都是文件的绝对路径的末尾文字; 如后缀： xml;.xml;abc.xml;js;.js;abc.js;
+ * 梁云-895423140@qq.com 文件操作都是以File类来进行操作， 所谓的后缀也都是文件的绝对路径的末尾文字; 如后缀：
+ * xml;.xml;abc.xml;js;.js;abc.js;
+ * 
  * @author sudy-liangyun
  *
  */
@@ -24,7 +24,9 @@ public class Wx2Ant {
 	private ArrayList<String> JSToRegexp = new ArrayList<>();
 	private ArrayList<String> AXMLRegexp = new ArrayList<>();
 	private ArrayList<String> AXMLToRegexp = new ArrayList<>();
-
+	private ArrayList<String> JSONRegexp = new ArrayList<>();
+	private ArrayList<String> JSONToRegexp = new ArrayList<>();
+	private String dir = "";
 	/**
 	 * 将符合后缀的文件copy和修改后缀名为指定的后缀。e.g.
 	 * addUpdateSuffix(".wxml",".axml");将会copy指定目录下所有的.wxml后缀的文件为.axml后缀的文件
@@ -50,98 +52,24 @@ public class Wx2Ant {
 	private int order = WX2ANT;
 
 	public static void main(String[] args) {
+
 		Wx2Ant us = new Wx2Ant();
-		String dir = "";// alibaba目录
-		System.out.println("工作目录："+System.getProperty("user.dir"));
-		String configPath=System.getProperty("user.dir")+File.separator+"wx2ali.txt";
-//		String configPath="z:/wx2ali.txt";
-		try {
-			LineIterator li=FileUtils.lineIterator(new File(configPath), "utf-8");
-			while(li.hasNext()){
-				String l=li.nextLine();
-				if("JSmethod".equals(l.trim())){
-					for(;li.hasNext();){
-						String nl=li.nextLine().trim();
-						if("end".equals(nl)){
-							break;
-						}
-						if(nl==""){
-							continue;
-						}
-						System.out.println(nl);
-						String[] aTob=nl.split("--->");
-						us.addUpdateMethods(aTob[0], aTob[1]);
-					}
-				}else if("JS".equals(l.trim())){
-					for(;li.hasNext();){
-						String nl=li.nextLine().trim();
-						if("end".equals(nl)){
-							break;
-						}
-						if(nl==""){
-							continue;
-						}
-						System.out.println(nl);
-						String[] aTob=nl.split("--->");
-						us.addJSRegexp(aTob[0], aTob[1]);
-					}
-				}else if("AXML".equals(l.trim())){
-					for(;li.hasNext();){
-						String nl=li.nextLine().trim();
-						if("end".equals(nl)){
-							break;
-						}
-						if(nl==""){
-							continue;
-						}
-						System.out.println(nl);
-						String[] aTob=nl.split("--->");
-						us.addAXMLRegexp(aTob[0], aTob[1]);
-					}
-				}else if("DIR".equals(l.trim())){
-					for(;li.hasNext();){
-						String nl=li.nextLine().trim();
-						if("end".equals(nl)){
-							break;
-						}
-						if(nl==""){
-							continue;
-						}
-						System.out.println(nl);
-						dir=nl;
-						System.out.println("修改文件的路径切换到："+nl);
-					}
-				}
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		us.addUpdateSuffix("wxml","axml");//这边的order是UPDATESUFFIX 所以是修改后缀名 wxml->axml
-		us.addUpdateSuffix("wxss","acss");//wxss->acss
+		System.out.println("工作目录：" + System.getProperty("user.dir"));
+		String configPath = System.getProperty("user.dir") + File.separator + "wx2ali.txt";
+		// String configPath = "z:/wx2ali.txt";
+		us.readConfig(configPath);
+		us.addUpdateSuffix("wxml", "axml");// 这边的order是UPDATESUFFIX 所以是修改后缀名
+											// wxml->axml
+		us.addUpdateSuffix("wxss", "acss");// wxss->acss
 		us.setOrder(UPDATESUFFIX);
-		us.HandleFile(dir);
+		us.HandleFile(us.dir);
 		us.clearSuffix();
-//		修改的文件后缀名
-		us.addUpdateSuffix("axml");//这边的order是WX2ANT 所以是修改该后缀名的文件
-		us.addUpdateSuffix("js");
-////		JSmethod
-//		us.addUpdateMethods("request", "httpRequest");//函数名转换request->httpRequest
-//		us.addUpdateMethods("login", "getAuthCode");//login->getAuthCode
-//		us.addUpdateMethods("showModal", "confirm");//showModal->confirm
-////		JS
-//		us.addJSRegexp("getStorageSync\\(\\s*(\\S+)\\s*\\)", "getStorageSync({key:$1})");//getStorageSync的参数形式转换
-//		us.addJSRegexp("setStorageSync\\(\\s*(\\S+)\\s*\\,\\s*(\\S+)\\s*\\)", "setStorageSync({key:$1,data:$2})");//setStorageSync的参数形式转换
-//		us.addJSRegexp("removeStorageSync\\(\\s*(\\S+)\\s*\\)", "removeStorageSync({key:$1})");//removeStorageSync的参数形式转换
-//		us.addJSRegexp("(\\S+)\\s*:\\s*function", "$1");//object中函数转为es6推荐的函数
-////		AXML
-//		us.addAXMLRegexp("(^|\\s+)wx:", "$1a:");//修改wx的标签变为a的标签
-//		us.addAXMLRegexp("(^|\\s+)bindtap(\\s*=)", "$1onTap$2");//修改bindtap为onTap
-//		us.addAXMLRegexp("(^|\\s+)bindlongTap(\\s*=)", "$1onLongTap$2");//修改bindlongTap为onLongTap
-//		us.addAXMLRegexp("(^|\\s+)bindinput(\\s*=)", "$1onInput$2");//修改bindinput为onInput
-//		us.addAXMLRegexp("(^|\\s+)bind(\\w+)(\\s*=)", "$1on$2$3");//修改bind为on
-//		动作
+		us.addUpdateSuffix(".axml");
+		us.addUpdateSuffix(".acss");
+		us.addUpdateSuffix(".json");
+		// 动作
 		us.setOrder(WX2ANT);
-		us.HandleFile(dir);
+		us.HandleFile(us.dir);
 	}
 
 	// -----------------------------------
@@ -211,6 +139,21 @@ public class Wx2Ant {
 	public void clearAXMLRegexp() {
 		this.AXMLRegexp.clear();
 		this.AXMLToRegexp.clear();
+	}
+
+	// ----------------------------------- json要更新的正则表达式
+	public void addJSONRegexp(String s) {
+		addAXMLRegexp(s, "");
+	}
+
+	public void addJSONRegexp(String suffix, String toSuffix) {
+		this.JSONRegexp.add(suffix);
+		this.JSONToRegexp.add(toSuffix);
+	}
+
+	public void clearJSONRegexp() {
+		this.JSONRegexp.clear();
+		this.JSONToRegexp.clear();
 	}
 	// -----------------------------------
 
@@ -340,15 +283,69 @@ public class Wx2Ant {
 			}
 		} else if (f.getAbsolutePath().endsWith(".axml")) {
 			try {
-				String s = FileUtils.readFileToString(f,"utf-8");
+				String s = FileUtils.readFileToString(f, "utf-8");
 				for (int i = 0; i < AXMLRegexp.size(); i++) {// 修改不一样的方法
 					s = s.replaceAll(AXMLRegexp.get(i), AXMLToRegexp.get(i));
 				}
-				FileUtils.writeStringToFile(f, s,"utf-8");
+				FileUtils.writeStringToFile(f, s, "utf-8");
 				System.out.println("转换axml文件：" + f.getAbsolutePath());
 			} catch (IOException e) {
 				System.out.println("转换axml文件出错：" + f.getAbsolutePath() + "--" + e.getMessage());
 			}
+		} else if (f.getAbsolutePath().endsWith(".json")) {
+			try {
+				String s = FileUtils.readFileToString(f, "utf-8");
+				for (int i = 0; i < JSONRegexp.size(); i++) {// 修改不一样的方法
+					s = s.replaceAll(JSONRegexp.get(i), JSONToRegexp.get(i));
+				}
+				FileUtils.writeStringToFile(f, s, "utf-8");
+				System.out.println("转换json文件：" + f.getAbsolutePath());
+			} catch (IOException e) {
+				System.out.println("转换json文件出错：" + f.getAbsolutePath() + "--" + e.getMessage());
+			}
+		}
+	}
+
+	public void readConfig(String configPath) {
+		try {
+			LineIterator li = FileUtils.lineIterator(new File(configPath), "utf-8");
+			int state = 0;
+			while (li.hasNext()) {
+				String l = li.nextLine();
+				l = l.replaceAll("#.*$", "").trim();
+				if (state == 0) {
+					if ("JSmethod".equals(l)) {
+						state = 1;
+					} else if ("JS".equals(l)) {
+						state = 2;
+					} else if ("AXML".equals(l)) {
+						state = 3;
+					} else if ("JSON".equals(l)) {
+						state = 4;
+					} else if ("DIR".equals(l)) {
+						state = 5;
+					}
+				} else if ("end".equals(l)) {
+					state = 0;
+				} else if (state == 1) {
+					String[] aTob = l.split("--->");
+					this.addUpdateMethods(aTob[0], aTob[1]);
+				} else if (state == 2) {
+					String[] aTob = l.split("--->");
+					this.addJSRegexp(aTob[0], aTob[1]);
+				} else if (state == 3) {
+					String[] aTob = l.split("--->");
+					this.addAXMLRegexp(aTob[0], aTob[1]);
+				} else if (state == 4) {
+					String[] aTob = l.split("--->");
+					this.addJSONRegexp(aTob[0], aTob[1]);
+				} else if (state == 5) {
+					dir = l;
+					System.out.println("切换到修改文件路径：" + l);
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 }
